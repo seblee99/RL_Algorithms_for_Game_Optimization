5b2d5e4218ffa72907d6e4290ceaf9fd2e531515
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2021-11-22 17:30:37 +0100

    Updated README and setup

--- README.md
+++ README.md
@@ -16,3 +16,38 @@ pip install text-flappy-bird-gym
 
 ## Usage
 
+```python
+
+import os, sys
+import gym
+import time
+
+import text_flappy_bird_gym
+
+if __name__ == '__main__':
+
+    # initiate environment
+    env = gym.make('TextFlappyBird-screen-v0', screen_size=(15, 11), pipe_gap = 4)
+    obs = env.reset()
+
+    # iterate
+    while True:
+
+        # Select next action
+        action = env.action_space.sample()  # for an agent, action = agent.policy(observation)
+
+        # Appy action and return new observation of the environment
+        obs, reward, done, info = env.step(action)
+
+        # Render the game
+        os.system("clear")
+        sys.stdout.write(env.render())
+        time.sleep(0.2) # FPS
+
+        # If player is dead break
+        if done:
+            break
+
+    env.close()
+```
+
--- setup.py
+++ setup.py
@@ -2,5 +2,28 @@ from setuptools import setup
 
 setup(name='text_flappy_bird_gym',
       version='0.0.1',
-      install_requires=['gym', 'numpy']  # And any other dependencies foo needs
+      author='Stergios C.',
+      description='Implementation of two OpenAI Gym learning environments of a simple unit-pixel player version of the Flappy Bird.',
+      install_requires=['gym', 'numpy'],  # And any other dependencies foo needs
+      license="MIT License",
+      python_requires=">=3.6",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
